cc7e220604d4cbbbe09550e455c447602b7c5746
Ermir <ermirmehukaj22@gmail.com>
2024-06-23 13:33:59 +0200

    Spring = API

---
+++ spring-17-REST-ConsumeApis/src/main/java/com/cydeo/dto/ResponseWrapper.java
@@ -0,0 +1,33 @@
+package com.cydeo.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class ResponseWrapper {
+
+
+    private boolean succsess;
+    private String message;
+    private Integer code;
+    private Object data;
+
+
+    public ResponseWrapper(String message, Object data) {
+        this.data = data;
+        this.message = message;
+        this.code= HttpStatus.OK.value();
+        this.succsess=true;
+
+    }
+
+    public ResponseWrapper(String message){
+        this.message=message;
+        this.code=HttpStatus.OK.value();
+        this.succsess=true;
+    }
+}
